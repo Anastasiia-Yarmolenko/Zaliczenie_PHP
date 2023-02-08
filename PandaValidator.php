74c8ca3260693515f10e64a664aa0415a51c489b
Anastasiia-Yarmolenko <99354597+Anastasiia-Yarmolenko@users.noreply.github.com>
2023-02-08 21:33:17 +0100

    Update PandaValidator.php

--- PandaValidator.php
+++ PandaValidator.php
@@ -1,43 +1,21 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-</head>
-<body>
-
 <?php
-    // define variables and set to empty values
-    $text1 = $text2="";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $text1 = test_input($_POST["text1"]);
-    $text2 = test_input($_POST["text2"]);
+namespace Apsl\Validator;
 
+class PandaValidator{
+    public function isValidText(string $text): bool
+    {
+        if($text == "panda")
+            return true;
+        else
+            return false;
     }
 
-    function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    public function isValidGrade($grade): bool
+    {
+        if($grade == 3)
+            return true;
+        else
+            return false;
     }
-?>
-
-<h2>PHP Form Validation Example</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-text1: <input type="text" name="text1">
-<br><br>
-text2: <input type="text" name="text2">
-<br><br>
-</form>
-
-<?php
-echo "<h2>Your Input:</h2>";
-echo $text1;
-echo "<br>";
-echo $text2;
-echo "<br>";
-
-?>
-
-</body>
-</html>
+}
